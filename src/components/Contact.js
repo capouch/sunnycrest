53624e5a9fb5056c5be33ebc2832768c3e12b389
Brian Capouch <brianc@palaver.net>
2018-03-23 14:25:22 -0400

    Set demographics

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,22 +28,22 @@ const Contact = (props) => (
                     <div className="contact-method">
                         <span className="icon alt fa-envelope"></span>
                         <h3>Email</h3>
-                        <a href="#">information@untitled.tld</a>
+                        <a href="#">brianc@palaver.net</a>
                     </div>
                 </section>
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-phone"></span>
                         <h3>Phone</h3>
-                        <span>(000) 000-0000 x12387</span>
+                        <span>(219) 253-8181</span>
                     </div>
                 </section>
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-home"></span>
                         <h3>Address</h3>
-                        <span>1234 Somewhere Road #5432<br />
-                        Nashville, TN 00000<br />
+                        <span>8806 N. SR 71<br />
+                        Raub, Indiana 47942<br />
                         United States of America</span>
                     </div>
                 </section>

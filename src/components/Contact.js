c053cd710cc1519ca483456edb317622bb2f1dd7
Brian Capouch <brianc@palaver.net>
2018-03-24 03:50:21 -0400

    Fix mailto link (still buggy in Brave)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,7 +29,7 @@ const Contact = (props) => (
                     <div className="contact-method">
                         <span className="icon alt fa-envelope"></span>
                         <h3>Email</h3>
-                        <a href="mailto:brianc@palaver.net">brianc@palaver.net</a>
+                        <a href="mailto:brianc@palaver.net?Subject=Email from Sunny Crest" target="_blank">brianc@palaver.net</a>
                     </div>
                 </section>
                 <section>
